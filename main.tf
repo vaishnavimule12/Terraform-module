b62ee61f133b1dfb1d4741f31811162d182e4cb2
vaishnavi mule <vaishnavimule128@gmail.com>
2026-06-18 18:54:23 +0530

    changed security group

--- main.tf
+++ main.tf
@@ -21,9 +21,8 @@ module "myinstance"{
     tag_name = var.tag_name
 }
 resource "aws_security_group" "my_sg" {
-  source = "./vpc"
   name   = "my-sg"
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = module.myvpc.vpc_id
 
   ingress {
     from_port   = 22
